4b5a23b76be70bf273be022506da0386c670e360
walterlv <xblero@gmail.com>
2020-10-26 16:40:17 +0800

    添加最简复现代码

---
+++ Walterlv.Issues.ShellCrash/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.WindowsAPICodePack.Shell;
+
+using Walterlv.Issues.ShellCrash.Utils;
+
+namespace Walterlv.Issues.ShellCrash
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentRendered += OnContentRendered;
+        }
+
+        private void OnContentRendered(object sender, EventArgs e)
+        {
+            ShowInjectedDlls();
+            ShowDesktopIcons();
+        }
+
+        private void ShowInjectedDlls()
+        {
+            foreach (ProcessModule module in Process.GetCurrentProcess().Modules.FindOutNotTrustfulModules())
+            {
+                DebugListBox.Items.Add(Path.GetFileName(module.FileName));
+            }
+        }
+
+        private void ShowDesktopIcons()
+        {
+            var deskPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            foreach (var item in new DirectoryInfo(deskPath).EnumerateFileSystemInfos())
+            {
+                using (var sf = ShellObject.FromParsingName(item.FullName))
+                {
+                    sf.Thumbnail.FormatOption = ShellThumbnailFormatOption.IconOnly;
+                    var bitmapSource = sf.Thumbnail.LargeBitmapSource;
+                    bitmapSource?.Freeze();
+                    DebugPanel.Children.Add(new Image
+                    {
+                        Source = bitmapSource,
+                        Stretch = Stretch.None,
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ Walterlv.Issues.ShellCrash/Utils/RemoteDllsBlacklist.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Walterlv.Issues.ShellCrash.Utils
+{
+    internal static class RemoteDllsBlacklist
+    {
+        private static readonly HashSet<string> _blacklist = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "gamebox_shell.dll",
+            "dopuslib32.dll",
+            "ListaryHook.dll",
+        };
+
+        private static readonly HashSet<string> _trustDirectorylist = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            @"C:\WINDOWS",
+            AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+        };
+
+        /// <summary>
+        /// 找到所有模块中可能不受信任的模块（非系统目录，非 EN 目录）。
+        /// </summary>
+        /// <param name="modules">模块列表。</param>
+        /// <returns>可能不受信任的模块（非系统目录，非 EN 目录）。</returns>
+        internal static IEnumerable<ProcessModule> FindOutNotTrustfulModules(this ProcessModuleCollection modules)
+        {
+            return modules.OfType<ProcessModule>().Where(x => _trustDirectorylist.All(
+                t => !x.FileName.StartsWith(t, StringComparison.OrdinalIgnoreCase)));
+        }
+
+        /// <summary>
+        /// 找到所有模块中已明确在黑名单中的模块。
+        /// </summary>
+        /// <param name="modules">模块列表。</param>
+        /// <returns>已明确在黑名单中的模块。</returns>
+        internal static IEnumerable<ProcessModule> FindOutBlacklistModules(this ProcessModuleCollection modules)
+        {
+            return modules.OfType<ProcessModule>().FindOutBlacklistModules();
+        }
+
+        /// <summary>
+        /// 找到指定的模块序列中已明确在黑名单中的模块。
+        /// </summary>
+        /// <param name="modules">模块列表。</param>
+        /// <returns>已明确在黑名单中的模块。</returns>
+        internal static IEnumerable<ProcessModule> FindOutBlacklistModules(this IEnumerable<ProcessModule> modules)
+        {
+            return modules.OfType<ProcessModule>().Where(x =>
+                _blacklist.Contains(Path.GetFileName(x.FileName), StringComparer.OrdinalIgnoreCase));
+        }
+    }
+}
